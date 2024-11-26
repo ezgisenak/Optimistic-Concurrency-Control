bd154ca547353318f7ce8bee11e74138fecc22a7
Ezgi Sena Karabacak <ezgisenakarabacak@10-192-50-63client.eduroam.upc.edu>
2024-11-26 11:15:43 +0100

    update entry.erl

--- entry.erl
+++ entry.erl
@@ -10,14 +10,14 @@ init(Value) ->
 entry(Value, Time) ->
     receive
         {read, Ref, From} ->
-            %% TODO: ADD SOME CODE
+            From ! {Ref, self(), Value, Time},
             entry(Value, Time);
         {write, New} ->
-            entry(... , ...);  %% TODO: COMPLETE
+            entry(New, make_ref()); 
         {check, Ref, Readtime, From} ->
             if 
-                 ... == ... ->   %% TODO: COMPLETE
-                    %% TODO: ADD SOME CODE
+                Readtime == Time ->
+                    From ! {Ref, ok};
                 true ->
                     From ! {Ref, abort}
             end,
